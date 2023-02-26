8395314c1ca61fa20db0345355fe85cba6693c4c
Sealkeen <lettersforlotro@gmail.com>
2023-02-26 20:29:02 +0300

    Added new Solution Version with NetStandard-2.0

---
+++ NWindowsKits/NWindowsKits/windef/structs/ХВНД.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NWindowsKits.windef.structs
+{
+    public class ХВНД
+    {
+        public static HWND Криейт(IntPtr птр)
+        {
+            return new HWND() { ptr = птр };
+        }
+    }
+}
